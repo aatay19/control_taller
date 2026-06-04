84558e2c95349cb80ec9f267f413ee06753f3069
aatay19 <156135806+aatay19@users.noreply.github.com>
2026-06-04 11:28:00 -0400

    añdido campos en el dashboard para resumen

--- libreria/views/main.py
+++ libreria/views/main.py
@@ -1,10 +1,20 @@
-import io
-from datetime import datetime
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from libreria.models import Entrada, Salida
 
 @login_required
 def index(request):
-    context = {}
+    maquinas_en_taller = Entrada.objects.filter(estado='en_taller').count()
+    maquinas_entregadas = Salida.objects.count()
+    
+    ultimas_ingresadas = Entrada.objects.select_related('cliente').order_by('-fecha')[:5]
+    ultimas_entregadas = Salida.objects.select_related('entrada', 'entrada__cliente').order_by('-fecha_entrega')[:5]
+    
+    context = {
+        'maquinas_en_taller': maquinas_en_taller,
+        'maquinas_entregadas': maquinas_entregadas,
+        'ultimas_ingresadas': ultimas_ingresadas,
+        'ultimas_entregadas': ultimas_entregadas,
+    }
     return render(request, 'index.html', context)
+
